a72f825ee9c5f79e23dde97930414efde07864ff
albertrocha <rocharocharocha95@yahoo.com>
2022-10-02 16:07:42 -0400

    first init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Item from './components/Item'
+import model3 from './assets/model-3-tesla.jpeg'
+import modelS from './assets/model-s-tesla.jpeg'
+import modelX from './assets/model-x-tesla.jpeg'
+import modelY from './assets/model-y-tesla.jpeg'
+import solarRoof from './assets/solarroof-tesla.jpeg'
+import solarPanel from './assets/solar-panels-tesla.jpeg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">      
+      <Header />
+      <div className="app__itemsContainer">
+      <Item
+          title='Model 3'
+          desc='Schedule a Touchless Test Drive'
+          descLink=''
+          backgroundImg={model3}
+          leftBtnTxt='CUSTOM ORDER'
+          leftBtnLink=''
+          rightBtnTxt='EXISTING INVENTORY'
+          rightBtnLink=''
+          twoButtons='true'
+          car='car'
+          id="modelS"
+          first
+        />
+        <Item 
+          title='Model Y'
+          desc="Money-back Gaurantee"
+          descLink=''
+          backgroundImg={modelX}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+        />
+        <Item 
+          title='Model S'
+          desc="Money-back Gaurantee"
+          descLink=''
+          backgroundImg={modelS}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+        />
+        <Item 
+          title='Model X'
+          desc="Money-back Gaurantee"
+          descLink=''
+          backgroundImg={modelY}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+        />
+        <Item 
+          title='Solar Panels'
+          desc="Money-back Gaurantee"
+          descLink=''
+          backgroundImg={solarRoof}
+          leftBtnTxt="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnTxt='EXISTING INVENTORY'
+          rightBtnLink=''
+          twoButtons='true'
+        />
+        <Item 
+          title='Solar Roof'
+          desc="Money-back Gaurantee"
+          descLink=''
+          backgroundImg={solarPanel}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+        />
+        <Footer />
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <h2>Tesla Home Page Clone <span className="span-one">Albert Rocha</span> 2022</h2>
+    </div>
+  );
+}
+
+export default Footer;
